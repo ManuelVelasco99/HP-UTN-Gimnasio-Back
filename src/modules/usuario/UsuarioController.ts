069a478d81ce73d777ea0b591ec2d5b8915ca548
SebastianBellaIt <70810659+SebastianBellaIt@users.noreply.github.com>
2023-10-11 21:28:03 -0300

    add_updateUsersController

--- src/modules/usuario/UsuarioController.ts
+++ src/modules/usuario/UsuarioController.ts
@@ -2,6 +2,7 @@ import { AppDataSource } from "../../data-source";
 import { Usuario           } from "../../entity/Usuario";
 import { Request       } from "express-serve-static-core";
 import { Response      } from "express-serve-static-core";
+import { Rol } from "../../entity/Rol";
 
 
 export class UsuarioController {
@@ -15,6 +16,7 @@ export class UsuarioController {
     public static async agregar(req : Request<any>, res : Response<any>) : Promise<void> {
         let usuario = new Usuario();
         let fechaHoy = new Date();
+        let idRol = req.body.idRol; 
           usuario.contrasenia = req.body.contrasenia;    
           usuario.dni = req.body.dni;
           usuario.nombre = req.body.nombre;
@@ -25,12 +27,39 @@ export class UsuarioController {
           usuario.fecha_nacimiento = req.body.fecha_nacimiento;
           usuario.email = req.body.email;
           usuario.estado = true;
+          usuario.rol = await AppDataSource.manager.findOneBy(Rol, {id:idRol} )
           usuario = await AppDataSource.manager.save(usuario);
-
+          
         res.json({
             data : usuario
         })
     }
+
+    public static async editar(req :Request<any>, res: Response<any> ) : Promise <void>{
+        let usuarioId = req.params.id;
+        let usuario = await AppDataSource.manager.findOneBy(Usuario, {id : usuarioId});
+        if(!usuario)
+            {
+                return;
+            }
+            usuario.dni = req.body.dni;
+            usuario.nombre = req.body.nombre;
+            usuario.apellido = req.body.apellido;
+            usuario.telefono = req.body.telefono;
+            
+            usuario.dni = req.body.dni;
+            usuario.fecha_nacimiento = req.body.fecha_nacimiento;
+            usuario.email = req.body.email;
+            
+            usuario = await AppDataSource.manager.save(usuario);
+            res.json
+            (
+                {
+                    data:usuario
+                }
+            )
+
+    }   
 
 
 
